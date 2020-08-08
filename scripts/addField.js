8c4e6cd5f98080e19ed2589961fcf14bf14fd66b
BielSantos <gabrielsantos1216.gs@gmail.com>
2020-08-08 12:55:44 -0300

    Finalizando a 3° aula

---
+++ scripts/addField.js
@@ -0,0 +1,12 @@
+// Procurar o botao
+document.querySelector("#add-time").addEventListener('click', cloneField)
+// Quando clicar no botao 
+
+// Executar uma acao
+
+function cloneField(){
+    console.log("Cheguei aqui")
+}
+//Duplicar os campos
+
+//
